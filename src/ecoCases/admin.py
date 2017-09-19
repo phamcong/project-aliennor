af0f123e7ee7c7c17918123c1c0df1b3529510bd
phamcong <ccuong.ph@gmail.com>
2017-09-19 12:25:31 +0200

    commit: update admin view, change views to generic View with IndexView and DetailView.

--- src/ecoCases/admin.py
+++ src/ecoCases/admin.py
@@ -2,5 +2,22 @@ from django.contrib import admin
 from .models import EcoCase, ESM
 # Register your models here.
 
-admin.site.register(EcoCase)
+
+class ESMInline(admin.TabularInline):
+    model = ESM
+    extra = 2
+
+
+class EcoCaseAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['ecocase_title',
+                           'ecocase_description', 'ecocase_characters']}),
+        ('Date information', {'fields': ['pub_date']})
+    ]
+    inlines = [ESMInline]
+    list_display = ['ecocase_title', 'pub_date']
+    list_filter = ['pub_date']
+
+
+admin.site.register(EcoCase, EcoCaseAdmin)
 admin.site.register(ESM)
--- src/ecoCases/views.py
+++ src/ecoCases/views.py
@@ -1,19 +1,43 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
+from django.urls import reverse
+from django.views import generic
 from .models import EcoCase, ESM
 
-def index(request):
+
+class IndexView(generic.ListView):
     '''
         Display five lastest ecocases by pub_date.
     '''
-    latest_ecocase_list = EcoCase.objects.order_by('-pub_date')[:5]
-    context = {'latest_ecocase_list': latest_ecocase_list}
-    return render(request, 'ecocases/index.html', context)
+    template_name = 'ecocases/index.html'
+    context_object_name = 'latest_ecocase_list'
 
-def detail(request, ecocase_id):
+    def get_queryset(self):
+        """Return the last five added ecocase. """
+        return EcoCase.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
     '''
         Display ecocase by ecocase_id
     '''
-    ecocase = get_object_or_404(EcoCase, pk=ecocase_id)
-    return render(request, 'ecocases/detail.html', {'ecocase': ecocase})
+    model = EcoCase
+    template_name = 'ecocases/detail.html'
+
+
+# def index(request):
+#     '''
+#         Display five lastest ecocases by pub_date.
+#     '''
+#     latest_ecocase_list = EcoCase.objects.order_by('-pub_date')[:5]
+#     context = {'latest_ecocase_list': latest_ecocase_list}
+#     return render(request, 'ecocases/index.html', context)
+
+
+# def detail(request, ecocase_id):
+#     '''
+#         Display ecocase by ecocase_id
+#     '''
+#     ecocase = get_object_or_404(EcoCase, pk=ecocase_id)
+#     return render(request, 'ecocases/detail.html', {'ecocase': ecocase})
