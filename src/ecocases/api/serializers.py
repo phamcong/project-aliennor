1dd6d5263e632000977e6645b10c6af55b791afb
Cong Cuong PHAM <ccuong.ph@gmail.com>
2017-09-26 02:05:06 +0200

    API part 02.

--- src/ecocases/api/serializers.py
+++ src/ecocases/api/serializers.py
@@ -1,13 +1,18 @@
 from rest_framework import serializers
-
+from tinymce.widgets import TinyMCE
 from ecocases.models import EcoCase  # form ..models import EcoCase
+from accounts.api.serializers import UserDisplaySerializer
 
 
 class EcoCaseModelSerializer(serializers.ModelSerializer):
+    user = UserDisplaySerializer()
+
     class Meta:
         model = EcoCase
         fields = [
+            'user',
             'ecocase_title',
             'ecocase_description',
             'ecocase_characters',
+            'ecocase_images',
         ]
